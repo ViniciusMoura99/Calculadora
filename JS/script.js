c25051abd2297175de79b05980dbbe2e792d4a66
ViniciusMoura99 <vinicius.lmoura@hotmail.com>
2023-10-09 19:12:27 -0300

    mostrando interações

--- JS/script.js
+++ JS/script.js
@@ -1,14 +1,35 @@
 /* Seleção de elemntos */
-const previousOperationText = document.querySelector("#orevious-opration");
-const currentOperationText = document.querySelector("#current-opration");
+const previousOperationText = document.querySelector("#previous-operation");
+const currentOperationText = document.querySelector("#current-operation");
 const buttons = document.querySelectorAll("#buttons-container button");
 
-console.log(buttons);
+//console.log(buttons);
 
 /* Lógica da aplicação */
 class Calculator {
+    /* elementos passados no constructor, depois os atributos com this */
+    constructor(previousOperationText,currentOperationText) {
+        this.previousOperationText = previousOperationText;
+        this.currentOperationText = currentOperationText;
+        /* valor impresso na tela */
+        this.currentOperation = "";
+        /* valor que o usuário vai digitar */
+    }
+    // adiciona o digito da calc 
+    addDigit(digit) {
+        // console.log(digit); teste
+        this.currentOperation = digit;
+        this.updateScreen();
+    }
 
+    // Muda o valores da calc
+    updateScreen() {
+        // os n° da operação atual dentro do TEXTO da operação atual
+        this.currentOperation.innerText += this.currentOperation;
+    }
 }
+
+const calc = new Calculator (previousOperationText, currentOperationText);
 
 /* eventos */
 
@@ -16,7 +37,11 @@ buttons.forEach((btn) => {
     btn.addEventListener("click", (e) => {
         const value = e.target.innerText;
         
-        /* || operador lógico OU */
+        /* || operador lógico OU /
+        /* operador + converte em número */
+        /* "." está simbolizando se é caractere/string 
+        pois ele tem que ser número ou caractere
+        basicamente: o if vai verificar se o value é n° ou caractere*/
         if(+value >= 0 || value === ".") {
             console.log(value);
         } else {
